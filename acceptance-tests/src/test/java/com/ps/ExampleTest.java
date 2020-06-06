51fcd21aabcfa4b2e29e6d976d2cf57cce11ba42
Andrew Morgan <andrewmo@backbase.com>
2020-06-06 16:51:37 +0200

    Added acceptance tests

---
+++ acceptance-tests/src/test/java/com/ps/ExampleTest.java
@@ -0,0 +1,23 @@
+package com.ps;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.web.client.RestTemplate;
+
+public class ExampleTest {
+
+  @Test
+  public void should() {
+    // Given
+    final RestTemplate restTemplate = new RestTemplate();
+
+    // When
+    String responseBody = restTemplate
+        .getForObject("http://ps-jx-overidden-pipeline-jx-staging.35.192.49.117.nip.io", String.class);
+
+    // Then
+    assertThat(responseBody).isEqualTo("Hello there, Cheese!");
+  }
+
+}
